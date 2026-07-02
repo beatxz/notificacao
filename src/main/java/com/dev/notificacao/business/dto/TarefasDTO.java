2d480d54b377c236dcfbb44f7748e1fe19e24b78
bea <164930551+beatxz@users.noreply.github.com>
2026-07-02 14:40:01 -0300

    Adicionados metodos de envio de email

---
+++ src/main/java/com/dev/notificacao/business/dto/TarefasDTO.java
@@ -0,0 +1,27 @@
+package com.dev.notificacao.business.dto;
+
+import com.dev.notificacao.business.dto.enums.StatusNotificacaoEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDTO {
+
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern= "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern= "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+
+}
---
+++ src/main/java/com/dev/notificacao/business/dto/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,5 @@
+package com.dev.notificacao.business.dto.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE,NOTIFICADO,CANCELADO
+}
